6defaf7cce69fa8d566bdd846730e7164c95d90d
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-06-25 22:19:06 +0500

    1.Running app

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,16 @@ import 'package:blog_flutter_getx/Routes/Routes_initializing.dart';
 import 'package:blog_flutter_getx/view/SplashScreen/splash_Screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 
 
-void main()  {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
