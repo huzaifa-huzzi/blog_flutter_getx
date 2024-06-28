4e84b602c0bbfa966aef6b678ea94843aebac810
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-06-28 15:28:33 +0500

    1.Storing image in Firebase

--- lib/view/Camera/Camera_Screen.dart
+++ lib/view/Camera/Camera_Screen.dart
@@ -1,8 +1,9 @@
-import 'package:blog_flutter_getx/Resources/Color/colors.dart';
-import 'package:blog_flutter_getx/view_model/Controller/Camera/CameraController.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import '../../Resources/Color/colors.dart';
+import '../../view_model/Controller/Camera/CameraController.dart';
 
 
 class CameraScreen extends StatefulWidget {
@@ -13,10 +14,12 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
-  final  cameraController = Get.put(CameraController());
+  final CameraController cameraController = Get.put(CameraController());
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.sizeOf(context).height * 1;
+    final width = MediaQuery.sizeOf(context).height * 1;
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -32,21 +35,50 @@ class _CameraScreenState extends State<CameraScreen> {
         centerTitle: true,
         backgroundColor: AppColor.pinkColor,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: Obx(() {
-              return cameraController.image.value == null
-                  ? Text(
-                'Select an Image',
-                style: GoogleFonts.lato(fontSize: 25, color: Colors.black),
-              )
-                  : Image.file(cameraController.image.value!);
-            }),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Obx(() {
+                if (cameraController.image.value == null) {
+                  return Text(
+                    'Select an Image',
+                    style: GoogleFonts.lato(
+                      fontSize: 25,
+                      color: Colors.black,
+                    ),
+                  );
+                } else {
+                  return Column(
+                    children: [
+                      Container(
+                        height: height * 0.3,
+                        width: width * 0.3,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                        ),
+                        child: Image.file(
+                          cameraController.image.value!,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Enter some text',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ],
+                  );
+                }
+              }),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.camera_enhance_rounded),
