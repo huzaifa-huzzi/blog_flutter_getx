65b20c8dd25cc2ea2735f51804d44ea63f68c26e
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-07-01 17:43:26 +0500

    1.Making ui in Homescreen

--- lib/view/Home/Home_Screen.dart
+++ lib/view/Home/Home_Screen.dart
@@ -1,86 +1,147 @@
-import 'package:blog_flutter_getx/Data/Exception/appException.dart';
-import 'package:blog_flutter_getx/Resources/Color/colors.dart';
-import 'package:blog_flutter_getx/Routes/Routes_name.dart';
-import 'package:blog_flutter_getx/Utils/Utils.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+import 'package:get/get.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 
+import '../../Data/Exception/appException.dart';
+import '../../Routes/Routes_name.dart';
+import '../../Utils/Utils.dart';
+import 'package:blog_flutter_getx/view_model/Controller/Camera/CameraController.dart';
+import 'package:blog_flutter_getx/view_model/Services/SessionManager.dart';
+import 'package:loading_indicator/loading_indicator.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   final auth = FirebaseAuth.instance;
-  String currentDate = DateFormat('MMMM dd, yyyy').format(DateTime.now());
-  String currentDateTime = DateFormat('EEEE ,HH:mm:ss').format(DateTime.now());
+  final databaseRef = FirebaseDatabase.instance.ref().child('user');
+  final cameraController = Get.put(CameraController());
+  final currentDateTime = DateFormat('MMM dd, yyyy').format(DateTime.now());
+  final currentTime = DateFormat('EEE, HH:mm:ss').format(DateTime.now());
+
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height * 1;
     final width = MediaQuery.of(context).size.width * 1;
-    return Scaffold(
 
+    return Scaffold(
       appBar: AppBar(
-        title: Text('Home Screen',style:GoogleFonts.lato(
-          textStyle: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            color: AppColor.whiteColor,
-          ),),),
+        title: Text(
+          'Home Screen',
+          style: GoogleFonts.lato(
+            textStyle: const TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
         centerTitle: true,
         actions: [
-          IconButton(onPressed: (){
-            auth.signOut().then((value){
-              Utils.snackBar('_logout'.tr,'_logout message'.tr);
-              Get.toNamed(RouteName.loginScreen);
-            }).onError((error,stackTrace){
-              ErrorException();
-            });
-          }, icon:  Icon(Icons.logout,color: AppColor.whiteColor,))
+          IconButton(
+            onPressed: () {
+              auth.signOut().then((value) {
+                Utils.snackBar('_logout'.tr, '_logout message'.tr);
+                Get.toNamed(RouteName.loginScreen);
+              }).catchError((error, stackTrace) {
+                ErrorException();
+              });
+            },
+            icon: const Icon(Icons.logout, color: Colors.white),
+          )
         ],
         automaticallyImplyLeading: false,
         backgroundColor: AppColor.pinkColor,
       ),
-      body: SafeArea(
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: height * .08 ),
-                child: Card(
+      body: SingleChildScrollView(
+        child: StreamBuilder(
+          stream: databaseRef.child(SessionManager().userId.toString()).onValue,
+          builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
+            if (!snapshot.hasData || snapshot.data == null) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasData) {
+              Map<dynamic, dynamic>? map = snapshot.data!.value as Map<dynamic, dynamic>?;
+
+              if (map != null) {
+                String? imageUrl = map['imageUrl'];
+                String? text = map['text'];
+
+                return SafeArea(
                   child: Column(
                     children: [
-                       Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                           Text('$currentDate',style:GoogleFonts.lato(
-                             textStyle: TextStyle(
-                               fontSize: 15,
-                               fontWeight: FontWeight.bold,
-                               color: AppColor.blackColor,
-                             ),),),
-                            Text('$currentDateTime',style:GoogleFonts.lato(
-                              textStyle: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold,
-                                color: AppColor.blackColor,
-                              ),),),
-                          ],
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: width * .02, vertical: height * .04),
+                        child: Card(
+                          elevation: 10,
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    '$currentDateTime',
+                                    style: GoogleFonts.lato(
+                                      textStyle: const TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.normal,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    '$currentTime',
+                                    style: GoogleFonts.lato(
+                                      textStyle: const TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.normal,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                              SizedBox(height: height * .02),
+                              Container(
+                                height: 200,
+                                width: 300,
+                                child: imageUrl != null
+                                    ? Image.network(imageUrl)
+                                    : const Center(child: Text('No Blog Available')),
+                              ),
+                              SizedBox(height: height * 0.02),
+                              Text(
+                                text ?? '',
+                                style: GoogleFonts.lato(
+                                  textStyle: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
+                      ),
                     ],
                   ),
-                ),
-              )
-            ],
-          )
+                );
+              } else {
+                return const Text('Data not available');
+              }
+            } else {
+              return const Text('Something went wrong');
+            }
+          },
+        ),
       ),
-
     );
   }
 }
--- lib/view_model/Controller/Camera/CameraController.dart
+++ lib/view_model/Controller/Camera/CameraController.dart
@@ -3,10 +3,9 @@ import 'package:blog_flutter_getx/Utils/Utils.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
-
+import 'package:flutter/material.dart';
 import '../../Services/SessionManager.dart';
 
 class CameraController extends GetxController {
@@ -34,14 +33,11 @@ class CameraController extends GetxController {
     }
   }
 
-  // Functions for the uploadImage
+  // Function to upload image to Firebase Storage and save data to Realtime Database
   Future<void> uploadImage() async {
-    if (image.value == null) {
-      Utils.snackBar('Error', 'Please select an image first');
-      return;
-    }
 
     loading.value = true;
+
     Reference ref = storage.ref().child('/ProfilePicture/${SessionManager().userId.toString()}');
 
     try {
@@ -50,13 +46,13 @@ class CameraController extends GetxController {
       String downloadUrl = await snapshot.ref.getDownloadURL();
 
       // Save the image URL and text to the database
-      await databaseRef.child('Profile Picture').set({
+      await databaseRef.child(SessionManager().userId.toString()).set({
         'imageUrl': downloadUrl,
         'text': controller.text,
       });
 
       if (kDebugMode) {
-        Utils.snackBar('_succes'.tr, '_suces blog'.tr);
+        print('Image and text uploaded successfully.');
       }
       loading.value = false;
     } catch (error) {
@@ -66,4 +62,5 @@ class CameraController extends GetxController {
       loading.value = false;
     }
   }
+
 }
